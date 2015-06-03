9a23af58bf8a28e64595b1b802429380403e99fe
Max Laponsky <thebiglaponsky@gmail.com>
2015-06-03 13:21:36 -0400

    Change REDIS_URL

--- config.py
+++ config.py
@@ -10,7 +10,10 @@ MONGODB_SETTINGS = {'db': 'PyDemic'}
 WTF_CSRF_ENEABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-REDIS_URL = "redis://:password@localhost:5000/0"
+try:
+    REDIS_URL = os.environ['REDISTOGO_URL']
+except KeyError:
+    REDIS_URL = "redis://:password@localhost:5000/0"
 
 FACEBOOK = {'id': '1634720196743881',
             'secret': 'c0127a9cb6c3e73beae751b7d8ff5155',

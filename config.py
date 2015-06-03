139dfd4cbc3e721f352cdd4ec655e4a354e15257
Max Laponsky <thebiglaponsky@gmail.com>
2015-06-03 12:52:40 -0400

    Added PostgreSQL database logging and Google sign-in

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-from authomatic.providers import oauth1, oauth2, openid
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 try:
@@ -16,7 +15,7 @@ REDIS_URL = "redis://:password@localhost:5000/0"
 FACEBOOK = {'id': '1634720196743881',
             'secret': 'c0127a9cb6c3e73beae751b7d8ff5155',
             'scope': ['email', 'name', 'id']}
-            
+
 GOOGLE = {'id': '371660565684-4jqrhi9bjdci61cvuba1ojm94hb1suao.apps.googleusercontent.com',
           'secret': 'iZ61BbRvU9jQCsY-7AA_EBhV',
           'scope': ['email', 'name', 'id']}

34c36fc3c0a51eab76b988c51169bc9461dc342d
Max Laponsky <thebiglaponsky@gmail.com>
2015-06-03 14:52:11 -0400

    >:( >:( >:(heroku database redis bullshit

--- config.py
+++ config.py
@@ -7,7 +7,10 @@ MONGODB_SETTINGS = {'db': 'PyDemic'}
 WTF_CSRF_ENEABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-REDIS_URL = os.environ['REDIS_URL']
+try:
+    REDIS_URL = os.environ['REDISTOGO_URL']
+except KeyError:
+    REDIS_URL = os.environ['REDIS_URL']
 
 FACEBOOK = {'id': '1634720196743881',
             'secret': 'c0127a9cb6c3e73beae751b7d8ff5155',
